ca7013872217232cc71438346b15d33185f31ca3
SamaaSaamy <sa10soma@gmail.com>
2022-07-02 21:03:04 +0200

    first commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# # This file is used for convenience of local development.
+# # DO NOT STORE YOUR CREDENTIALS INTO GIT
+# export POSTGRES_USERNAME=postgres
+# export POSTGRES_PASSWORD=B46uzibdunqGukKVZ0V4
+# export POSTGRES_HOST=database-1.chvspjg4mv1s.us-east-1.rds.amazonaws.com
+# export POSTGRES_DB=udagram
+# export AWS_BUCKET=arn:aws:s3:::udacityprojectudagram
+# export AWS_REGION=us-east-1
+# export AWS_PROFILE=default
+# export JWT_SECRET=mysecretstring
+# export URL=http://udacityprojectudagram.s3-website-us-east-1.amazonaws.com
